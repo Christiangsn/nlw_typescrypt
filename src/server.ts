dc09e8bc3aba814c859f38343f9ca97b05e6d32d
Christiangsn <christian_gs159@hotmail.com>
2021-04-25 17:26:23 -0300

    on/emit admin

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import { http } from "./http";
 import "./websocket/client";
 import "./websocket/admin";
 
-
-
 const port = 3000
 
 http.listen(port, () => 
--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -1,13 +1,34 @@
 import { io } from "../http";
 import { ConnectionsServices } from "../services/ConnectionServices";
+import { MessagesServices } from "../services/MessagesServices";
 
 
 io.on("connect", async (socket) => { 
     const connectionService = new ConnectionsServices();
+    const messagesServices = new MessagesServices();
 
     const allConnectionsWithoutAdmin = await connectionService.findAllWithoutAdmin();
 
     io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+
+    socket.on("admin_list_messages_by_user", async (params, cb) => {
+        const { user_id } = params
+        
+        const allMessages = await messagesServices.messageByUser(user_id);
+        cb(allMessages)
+    })
+
+    socket.on("admin_send_message", async (params) => {
+        const { user_id, text } = params;
+
+        await messagesServices.store({
+            text,
+            user_id,
+            admin_id: socket.id
+        });
+
+        const connection = await connectionService.findByUserId(user_id)
+    })
 
 
 })
